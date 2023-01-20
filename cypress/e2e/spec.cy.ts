bf2159e613d25deefd52be6945c3414334783e3c
Rodrigo Nunes <imrodrigon@gmail.com>
2023-01-20 12:15:58 -0400

    test: add test error

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -13,4 +13,28 @@ describe("App", () => {
     cy.contains("13824");
     cy.contains("14400");
   });
+
+  it("should not be able to calculate a trasition anticipation with amount less than 1000", () => {
+    cy.visit("http://localhost:5173/");
+
+    cy.get("input[name=amount]").type("200");
+    cy.get("input[name=installments]").type("3");
+    cy.get("input[name=mdr]").type("4");
+
+    cy.get("form").submit();
+
+    cy.contains("​Valor da venda deve ser maior ou igual a 1000");
+  });
+
+  it("should not be able to calculate a trasition anticipation with installments more then 12", () => {
+    cy.visit("http://localhost:5173/");
+
+    cy.get("input[name=amount]").type("15000");
+    cy.get("input[name=installments]").type("13");
+    cy.get("input[name=mdr]").type("4");
+
+    cy.get("form").submit();
+
+    cy.contains("Número de parcelas deve ser entre 1 e 12");
+  });
 });
